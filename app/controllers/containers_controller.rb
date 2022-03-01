49d306a574de34fc11c3135d74751157d4030584
maxence tarnaud <maxence.trnd@gmail.com>
2022-03-01 16:39:57 +0100

    seed

--- app/controllers/containers_controller.rb
+++ app/controllers/containers_controller.rb
@@ -1,2 +1,11 @@
 class ContainersController < ApplicationController
+
+    def index
+       @containers = policy_scope(Container)
+    end
+
+    def new
+        @container = Container.new
+        authorize @container
+    end
 end
