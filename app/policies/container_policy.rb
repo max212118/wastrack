f8ced700b8d1a936150f1b476c0d1955fd56b23c
maxence tarnaud <maxence.trnd@gmail.com>
2022-03-02 12:14:56 +0100

    r

--- app/policies/container_policy.rb
+++ app/policies/container_policy.rb
@@ -2,7 +2,7 @@ class ContainerPolicy < ApplicationPolicy
   class Scope < Scope
     # NOTE: Be explicit about which records you allow access to!
     def resolve
-      # scope.where(city_hall_id: user.city_hall_id)
+      scope.where(city_hall_id: user.city_hall_id)
       scope.all
     end
   end
