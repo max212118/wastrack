3933e5d4fce415fdb3a7e63f9e6a9416e466d9b8
Aris Gyalui <arisdgyalui@outlook.com>
2022-03-03 13:14:01 +0100

    trucks

--- config/routes.rb
+++ config/routes.rb
@@ -12,5 +12,4 @@ Rails.application.routes.draw do
     resources :road_containers, only: [:update, :show]
     resources :trucks, only: [:show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-
 end
