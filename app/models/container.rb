72133cd1381e3d48314bb5496a51ec3a2cea2150
maxence tarnaud <maxence.trnd@gmail.com>
2022-03-09 12:44:03 +0100

    new trash colors for dashboard + link problems to fire

--- app/models/container.rb
+++ app/models/container.rb
@@ -8,7 +8,7 @@ class Container < ApplicationRecord
   after_validation :geocode, if: :will_save_change_to_location?
 
   def map_icon
-    return "garbage-fire.png" if temperature > 50
+    return "garbage-fire-2.png" if temperature > 50
     return "garbage-orange.png" if filling_level >= 80 && filling_level < 90
     return "garbage_red.png" if filling_level >= 90
     return "garbage_green.png"
