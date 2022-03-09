f5608d1a1fe5ff653e2b708b18d8d6f846d0e145
Aris Gyalui <arisdgyalui@outlook.com>
2022-03-09 10:58:59 +0100

    updates

--- app/javascript/controllers/chatroom_subscription_controller.js
+++ app/javascript/controllers/chatroom_subscription_controller.js
@@ -4,12 +4,14 @@ import consumer from "../channels/consumer"
 export default class extends Controller {
   static values = { chatroomId: Number }
   static targets = ["messages", "form"]
+  // received: data => this.#insertMessageScrollDownAndResetForm(data)
 
   connect() {
     this.channel = consumer.subscriptions.create(
       { channel: "ChatroomChannel", id: this.chatroomIdValue },
-      { received: data => this.#insertMessageAndScrollDown(data) }
-    )
+      // { received: data => this.#insertMessageAndScrollDown(data) }
+      { received: data => this.#insertMessageScrollDownAndResetForm(data) }
+      )
     console.log(`Subscribe to the chatroom with the id ${this.chatroomIdValue}.`)
   }
 
@@ -24,6 +26,11 @@ export default class extends Controller {
     this.channel.unsubscribe()
   }
 
+  #insertMessageScrollDownAndResetForm(data) {
+    this.messagesTarget.insertAdjacentHTML("beforeend", data)
+    this.messagesTarget.scrollTo(0, this.messagesTarget.scrollHeight)
+    this.formTarget.reset()
+  }
 }
 
 // look at method to deploy on heroku
